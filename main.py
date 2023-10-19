5e7cd3b472ac772016b8ba6d3802c67a85a33eb4
groovinflannel <groovinflannel@gmail.com>
2023-10-19 15:36:53 -0400

    created for loop to create snake's body with three Turtle objects as body segments; square shape, white color, x coordinate is -20 from previous body segment

--- main.py
+++ main.py
@@ -6,4 +6,15 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Perry the Python (Snake Game)")
 
+# create initial snake body
+snake = []
+for turtle in range(0, 3):
+    turt = Turtle(shape="square")
+    turt.color("white")
+    snake.append(turt)
+    current_index = snake.index(turt)
+    left_spacing = -20;
+    turt.setx(current_index * left_spacing)
+
+
 screen.exitonclick()
