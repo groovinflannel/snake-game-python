b17d976e78888bb7efc487448d0d49d22b19cd7d
groovinflannel <groovinflannel@gmail.com>
2023-10-19 15:05:30 -0400

    Screen size, background color, and title changed

---
+++ main.py
@@ -0,0 +1,9 @@
+from turtle import Turtle, Screen
+
+# screen setup
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Perry the Python (Snake Game)")
+
+screen.exitonclick()
